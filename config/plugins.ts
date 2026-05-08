69c354f9998fd826656404abbe690a55b4451c86
Nnzo8 <ninorollaneocliasa684@gmail.com>
2026-05-08 13:43:10 +0800

    feat: configure AWS S3 provider for file uploads and add necessary dependencies

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,26 @@
-import type { Core } from '@strapi/strapi';
-
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
-
-export default config;
+// config/plugins.ts
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          credentials: {
+            accessKeyId: env("SUPABASE_ACCESS_KEY_ID"),
+            secretAccessKey: env("SUPABASE_SECRET_ACCESS_KEY"),
+          },
+          region: env("SUPABASE_REGION"),
+          endpoint: env("SUPABASE_ENDPOINT"),
+          params: {
+            Bucket: env("SUPABASE_BUCKET"),
+          },
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
